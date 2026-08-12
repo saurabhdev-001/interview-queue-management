f6eeb5d7af637de05a6bab401fdb0b589d2d59cb
Saurabh <25112cn125@glbitm.ac.in>
2026-08-12 16:56:21 +0530

    Initial spring boot setup

---
+++ src/main/java/com/vividhita/interview_queue/InterviewQueueApplication.java
@@ -0,0 +1,13 @@
+package com.vividhita.interview_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vividhita/interview_queue/InterviewQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vividhita.interview_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
